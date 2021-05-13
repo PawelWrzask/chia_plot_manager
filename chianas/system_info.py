685b5519ee0bca1650299f5ffcd441a45653700f
PawelWrzask <wrzask.pawel@gmail.com>
2021-05-13 20:39:42 +0200

    Update system_info.py

--- chianas/system_info.py
+++ chianas/system_info.py
@@ -8,12 +8,12 @@ system_info.py for use with drive_manager.py
 VERSION = "V0.2 (2021-03-23)"
 
 ## Set Notification Accounts#
-alert_email = ['email_one@gmail.com', 'email_two@gmail.com']
+alert_email = ['xx', 'xx']
 twilio_from = '+10000000'
 twilio_account = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-twilio_token = 'kjshdkjhfkjshlfkjhkajhfkjahsfdkj'
-twilio_to = ['+17763540098', '+17763540086']
-pushbilletAPI = "o.khjsdfiuwyifkjshkjshkajhdiurieuiufqw"  # pushbullet API token (http://www.pushbullet.com)
+twilio_token = 'xx'
+twilio_to = ['xx', 'xx']
+pushbilletAPI = "xx"  # pushbullet API token (http://www.pushbullet.com)
 
 
 def main():
